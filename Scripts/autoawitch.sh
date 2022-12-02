e5fa4438c303637129770b04418470639eb9a224
ALbur <24468956+ALbur@users.noreply.github.com>
2022-12-02 19:16:46 +0800

    练手小脚本

---
+++ Scripts/autoawitch.sh
@@ -0,0 +1,12 @@
+#!/bin/ash
+target="USB 10" #eth0/USB10/LAN 
+echo host > /sys/kernel/debug/usb/ci_hdrc.0/role
+sleep 5
+result=$(dmesg lsusb | grep "${target}")
+if [[ "$result" != "" ]]
+then
+    echo "done"
+else
+    echo gadget > /sys/kernel/debug/usb/ci_hdrc.0/role
+    echo "false"
+fi
