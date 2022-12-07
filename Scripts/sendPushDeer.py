93edbfc7b13365dcba38bab6a6b0c96fb1299b55
ALbur <24468956+ALbur@users.noreply.github.com>
2022-12-07 15:27:40 +0800

    Update sendPushDeer.py

--- Scripts/sendPushDeer.py
+++ Scripts/sendPushDeer.py
@@ -5,9 +5,10 @@
 import os
 from pypushdeer import PushDeer   #pip install pypushdeer
 
-def getIPv6Address():
+def command():
     result= os.popen("命令").read()
     return result
 
+# pushdeer = PushDeer(server="server:8800", pushkey="key") # 自建pushdeer推送服务器 修改pushkey
 pushdeer = PushDeer(pushkey="pushkey")  # 默认服务器 修改pushkey
-pushdeer.send_text(getIPv6Address())
+pushdeer.send_text(command())
